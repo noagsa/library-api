a03e921a4414385f5d46b11b8fec177daa76f4be
Noa <noasalgadodev@gmail.com>
2026-03-30 07:51:22 +0200

    feat(config) add security filter chain

--- src/main/java/io/github/noagsa/libraryapi/config/SecurityConfig.java
+++ src/main/java/io/github/noagsa/libraryapi/config/SecurityConfig.java
@@ -2,11 +2,28 @@ package io.github.noagsa.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        String[] allowedPaths = {"/api/library/users"};
+
+        httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, allowedPaths).permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin.disable())
+                .csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
